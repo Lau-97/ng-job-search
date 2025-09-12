a40590bbe7de8355d899b5bb9b6f3a70437b1fad
Laura Maggi <lamaggilaura@gmail.com>
2025-09-12 11:17:24 +0200

    Details component

--- src/app/job-details/job-details.component.ts
+++ src/app/job-details/job-details.component.ts
@@ -1,12 +1,31 @@
 import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterLink, ActivatedRoute } from '@angular/router';
+import { switchMap, of } from 'rxjs';
+import { JobService } from '../job.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-job-details',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatIconModule],
   templateUrl: './job-details.component.html',
   styleUrl: './job-details.component.css',
 })
 export class JobDetailsComponent {
+  private jobService = inject(JobService);
+  private route = inject(ActivatedRoute);
+  job = toSignal(
+    this.route.paramMap.pipe(
+      switchMap((params) => {
+        const id = params.get('id');
+        if (!id) return of(undefined);
+        console.log("id", id)
+        return this.jobService.getDetails(parseInt(id));
+      })
+    ),
+    { initialValue: undefined }
+  );
+
 
 }
